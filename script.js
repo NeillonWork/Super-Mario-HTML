71e4a9930332541722389fe144654d2186a09d68
Neillon <neillonwork@gmail.com>
2022-07-20 22:16:36 -0300

    adicionado nuvens

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const mario = document.querySelector('.mario-correndo')
 const cano = document.querySelector('.cano')
 const chao = document.querySelector('.chao')
+const planice = document.querySelector('.planice')
+
+const nuvens01 = document.querySelector('.nuvens-01')
+const nuvens02 = document.querySelector('.nuvens-02')
+const nuvens03 = document.querySelector('.nuvens-03')
+const nuvens04 = document.querySelector('.nuvens-04')
+const nuvens05 = document.querySelector('.nuvens-05')
 
 const pulo = () => {
     mario.classList.add('pulo');
@@ -13,6 +20,15 @@ const pulo = () => {
 const repeticao = setInterval(() => {
 
     const posicaoDoCano = cano.offsetLeft;
+
+    const posicaoDaNuvem01 = nuvens01.offsetLeft;
+    const posicaoDaNuvem02 = nuvens02.offsetLeft;
+    const posicaoDaNuvem03 = nuvens03.offsetLeft;
+    const posicaoDaNuvem04 = nuvens04.offsetLeft;
+    const posicaoDaNuvem05 = nuvens05.offsetLeft;
+
+
+
     //computa a posicao bottom do mario ao longo do pulo, o sinal de + esta convertendo a string para number
     const posicaoDoMario = +window.getComputedStyle(mario).bottom.replace('px', '');
 
@@ -28,6 +44,29 @@ const repeticao = setInterval(() => {
 
         //para animacao do chão
         chao.style.animation = 'none';
+
+        //para animacao do chão
+        planice.style.animation = 'none';
+
+        //para animacao da nuvem - 01
+        nuvens01.style.left = `${posicaoDaNuvem01}px`;
+        nuvens01.style.animation = 'none';
+
+        //para animacao da nuvem - 02
+        nuvens02.style.left = `${posicaoDaNuvem02}px`;
+        nuvens02.style.animation = 'none';
+
+        //para animacao da nuvem - 03
+        nuvens03.style.left = `${posicaoDaNuvem03}px`;
+        nuvens03.style.animation = 'none';
+
+        //para animacao da nuvem - 04
+        nuvens04.style.left = `${posicaoDaNuvem04}px`;
+        nuvens04.style.animation = 'none';
+
+        //para animacao da nuvem - 05
+        nuvens05.style.left = `${posicaoDaNuvem05}px`;
+        nuvens05.style.animation = 'none';
 
         //Troca imagem para o mario morrendo
         mario.src = './images/gameOver.gif';
