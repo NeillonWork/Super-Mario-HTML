5d95b092b37e8a8af437df9378773c8eb0660bc8
Neillon <neillonwork@gmail.com>
2022-07-22 00:55:30 -0300

    updates

---
+++ index.js
@@ -0,0 +1,18 @@
+const nostalgia = document.querySelector('#audioId');
+const backImage = document.querySelector('#bgNostalgia');
+
+function playNostalgia(){
+    nostalgia.style.display= ('flex')
+    audioId.play();
+
+backImage.classList.add("nostalgia-body");
+   // backImage.style.backgroundImage = "url('./images/pixel-jeff-mario.gif')" opacity=("0.5");
+   
+
+console.log("OK")
+}
+
+
+
+
+
