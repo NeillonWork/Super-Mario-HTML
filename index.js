a92335485d476d6552ee02bf3f7d15ee52633ded
Neillon <neillonwork@gmail.com>
2022-07-22 05:16:57 -0300

    fundo

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const nostalgia = document.querySelector('#audioId');
-const backImage = document.querySelector('#bgNostalgia');
+const nostalgia  = document.querySelector('#audioId');
+const backImage  = document.querySelector('#bgNostalgia');
+const backImage2 = document.querySelector('#bgNostalgia2');
+const body       = document.querySelector('body');
 
 function playNostalgia(){
     nostalgia.style.display= ('flex')
     audioId.play();
 
 backImage.classList.add("nostalgia-body");
-   // backImage.style.backgroundImage = "url('./images/pixel-jeff-mario.gif')" opacity=("0.5");
+body.style.backgroundColor =('black');
+
+backImage2.classList.add("nostalgia-body2");
+backImage2.style.backgroundImage = "url('images/pixel-jeff-mario.gif')";
    
 
-console.log("OK")
+backImage2.style.backgroundColor=    ('black');
+backImage2.style.backgroundPosition= ('center');
+backImage2.style.backgroundRepeat=   ('no-repeat');
+backImage2.style.backgroundSize=     ('cover');
+backImage2.style.animation =         ('fadeIn 25s');
+backImage.style.animation =         ('fadeInNostalgia 3s');
+
 }
-
-
-
-
 
